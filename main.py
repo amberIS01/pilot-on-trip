65d9c5b3475caa1382f6dce8acb53877c4173c8e
amberIS01 <sahilsingh8310@gmail.com>
2025-07-25 19:39:05 +0530

    Docker

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from routers import user, location, vehicle, partner, service, dashboard, partner_query, customer_support, rider, booking, payment
 
 app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"message": "Welcome"}
+
 app.include_router(user.router)
 app.include_router(location.router)
 app.include_router(vehicle.router)
@@ -13,3 +17,4 @@ app.include_router(customer_support.router)
 app.include_router(rider.router)
 app.include_router(booking.router)
 app.include_router(payment.router)
+
